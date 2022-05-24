a7ad3002d9d2795bf956932c3233a0f4d05da17b
uOung <youyoung0703@gmail.com>
2022-05-24 16:28:11 +0900

    ⚡️ feat: N과 M(10)

---
+++ 15664.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[10], num[10];
+bool isused[10];
+
+void func(int k) {
+    if(k == m) {
+        for(int i = 0; i < m; i++) cout << num[arr[i]] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int tmp = -1;
+    int st = 0;
+    if(k != 0) st = arr[k - 1] + 1;
+    for(int i = st; i < n; i++) {
+        if(isused[i] || tmp == num[i]) continue;
+
+        isused[i] = true;
+        arr[k] = i;
+        func(k + 1);
+        isused[i] = false;
+        tmp = num[arr[k]];
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> num[i];
+
+    sort(num, num + n);
+
+    func(0);
+
+    return 0;
+}
