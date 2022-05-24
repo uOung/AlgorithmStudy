bbda434e9814cb5dc0b64467ea4e2dbfca0e61b5
uOung <youyoung0703@gmail.com>
2022-05-24 15:16:44 +0900

    ⚡️ feat: N과 M(4)

---
+++ 15652.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+
+void func(int k) {
+    if(k == m) {
+        for(int i = 0; i < m; i++) cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int st = 1;
+    if(k != 0) st = arr[k - 1];
+    for(int i = st; i <= n; i++) {
+        arr[k] = i;
+        func(k + 1);
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    func(0);
+
+    return 0;
+}
