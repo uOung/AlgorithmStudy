64320af9c45a4ab135fa4bcd1cad7765516e5a55
uOung <youyoung0703@gmail.com>
2022-05-24 16:03:59 +0900

    ⚡️ feat: N과 M(8)

---
+++ 15657.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[10], nums[10];
+
+void func(int k) {
+    if(k == m) {
+        for(int i = 0; i < m; i++) cout << nums[arr[i]] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int st = 0;
+    if(k != 0) st = arr[k - 1];
+    for(int i = st; i < n; i++) {
+        arr[k] = i;
+        func(k + 1);
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> nums[i];
+
+    sort(nums, nums + n);
+
+    func(0);
+
+    return 0;
+}
